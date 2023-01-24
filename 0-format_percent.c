0deed09580d28747adadbd575aa358eab05779a2
chardso <wafelaandrew2@gmail.com>
2023-01-24 19:51:07 +0300

    Update 0-format_percent.c

--- 0-format_percent.c
+++ 0-format_percent.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * print_perc - prints the percent(%) character
+ * print_perc - prints the percent(%) character.
  * @c: character to print
- * @buffer: pointer to current buffer
- * @length: amount of characters in current buffer
+ * @buffer: pointer to current buffer.
+ * @length: amount of characters in current buffer.
  * Return: On success 1.
  */
 int print_perc(va_list c __attribute__((unused)), char *buffer, unsigned int length)
