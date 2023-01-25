39b3a5aa9b32c053e06227fced894fcdd5e80c0e
chardso <wafelaandrew2@gmail.com>
2023-01-25 12:41:57 +0300

    Update 0-format_octal.c

--- 0-format_octal.c
+++ 0-format_octal.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_oct - prints decimal integer in base 8.
- * @list: list of arguments in this case, integers.
- * @buffer: pointer to buffer.
- * @length: amount of characters in current buffer.
- * Return: number of chars printed.
+ * print_oct - prints decimal integer in base 8
+ * @list: list of arguments in this case, integers
+ * @buffer: pointer to buffer
+ * @length: amount of characters in current buffer
+ * Return: number of chars printed
  */
 int print_oct(va_list list, char *buffer, unsigned int length)
 {
