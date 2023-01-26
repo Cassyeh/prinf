ed7a0cf753bfd0b216061dbf074bff2e071b0fcd
chardso <wafelaandrew2@gmail.com>
2023-01-26 04:54:57 +0300

    task 3

--- base_changes.c
+++ base_changes.c
@@ -9,37 +9,37 @@ int hex_check(int, char);
  */
 int print_binary(va_list list)
 {
-unsigned int num;
-int i, len;
-char *str;
-char *rev_str;
+	unsigned int num;
+	int i, len;
+	char *str;
+	char *rev_str;
 
-num = va_arg(list, unsigned int);
-if (num == 0)
-return (_write_char('0'));
-if (num < 1)
-return (-1);
-len = base_len(num, 2);
-str = malloc(sizeof(char) * len + 1);
-if (str == NULL)
-return (-1);
+	num = va_arg(list, unsigned int);
+	if (num == 0)
+		return (_write_char('0'));
+	if (num < 1)
+		return (-1);
+	len = base_len(num, 2);
+	str = malloc(sizeof(char) * len + 1);
+	if (str == NULL)
+		return (-1);
 
-for (i = 0; num > 0; i++)
-{
-if (num % 2 == 0)
-str[i] = '0';
-else
-str[i] = '1';
-num = num / 2;
-}
-str[i] = '\0';
-rev_str = rev_string(str);
-if (rev_str == NULL)
-return (-1);
-write_base(rev_str);
-free(str);
-free(rev_str);
-return (len);
+	for (i = 0; num > 0; i++)
+	{
+		if (num % 2 == 0)
+			str[i] = '0';
+		else
+			str[i] = '1';
+		num = num / 2;
+	}
+	str[i] = '\0';
+	rev_str = rev_string(str);
+	if (rev_str == NULL)
+		return (-1);
+	write_base(rev_str);
+	free(str);
+	free(rev_str);
+	return (len);
 }
 
 /**
@@ -49,37 +49,37 @@ return (len);
  */
 int print_octal(va_list list)
 {
-unsigned int num;
-int len;
-char *octal_rep;
-char *rev_str;
+	unsigned int num;
+	int len;
+	char *octal_rep;
+	char *rev_str;
 
-num = va_arg(list, unsigned int);
+	num = va_arg(list, unsigned int);
 
-if (num == 0)
-return (_write_char('0'));
-if (num < 1)
-return (-1);
-len = base_len(num, 8);
+	if (num == 0)
+		return (_write_char('0'));
+	if (num < 1)
+		return (-1);
+	len = base_len(num, 8);
 
-octal_rep = malloc(sizeof(char) * len + 1);
-if (octal_rep == NULL)
-return (-1);
-for (len = 0; num > 0; len++)
-{
-octal_rep[len] = (num % 8) + 48;
-num = num / 8;
+	octal_rep = malloc(sizeof(char) * len + 1);
+	if (octal_rep == NULL)
+		return (-1);
+	for (len = 0; num > 0; len++)
+	{
+		octal_rep[len] = (num % 8) + 48;
+		num = num / 8;
 
-}
-octal_rep[len] = '\0';
-rev_str = rev_string(octal_rep);
-if (rev_str == NULL)
-return (-1);
+	}
+	octal_rep[len] = '\0';
+	rev_str = rev_string(octal_rep);
+	if (rev_str == NULL)
+		return (-1);
 
-write_base(rev_str);
-free(octal_rep);
-free(rev_str);
-return (len);
+	write_base(rev_str);
+	free(octal_rep);
+	free(rev_str);
+	return (len);
 }
 
 /**
@@ -89,42 +89,42 @@ return (len);
  */
 int print_hex(va_list list)
 {
-unsigned int num;
-int len;
-int rem_num;
-char *hex_rep;
-char *rev_hex;
+	unsigned int num;
+	int len;
+	int rem_num;
+	char *hex_rep;
+	char *rev_hex;
 
-num = va_arg(list, unsigned int);
+	num = va_arg(list, unsigned int);
 
-if (num == 0)
-return (_write_char('0'));
-if (num < 1)
-return (-1);
-len = base_len(num, 16);
-hex_rep = malloc(sizeof(char) * len + 1);
-if (hex_rep == NULL)
-return (-1);
-for (len = 0; num > 0; len++)
-{
-rem_num = num % 16;
-if (rem_num > 9)
-{
-rem_num = hex_check(rem_num, 'x');
-hex_rep[len] = rem_num;
-}
-else
-hex_rep[len] = rem_num + 48;
-num = num / 16;
-}
-hex_rep[len] = '\0';
-rev_hex = rev_string(hex_rep);
-if (rev_hex == NULL)
-return (-1);
-write_base(rev_hex);
-free(hex_rep);
-free(rev_hex);
-return (len);
+	if (num == 0)
+		return (_write_char('0'));
+	if (num < 1)
+		return (-1);
+	len = base_len(num, 16);
+	hex_rep = malloc(sizeof(char) * len + 1);
+	if (hex_rep == NULL)
+		return (-1);
+	for (len = 0; num > 0; len++)
+	{
+		rem_num = num % 16;
+		if (rem_num > 9)
+		{
+			rem_num = hex_check(rem_num, 'x');
+			hex_rep[len] = rem_num;
+		}
+		else
+			hex_rep[len] = rem_num + 48;
+		num = num / 16;
+	}
+	hex_rep[len] = '\0';
+	rev_hex = rev_string(hex_rep);
+	if (rev_hex == NULL)
+		return (-1);
+	write_base(rev_hex);
+	free(hex_rep);
+	free(rev_hex);
+	return (len);
 }
 
 
@@ -135,42 +135,42 @@ return (len);
  */
 int print_heX(va_list list)
 {
-unsigned int num;
-int len;
-int rem_num;
-char *hex_rep;
-char *rev_hex;
+	unsigned int num;
+	int len;
+	int rem_num;
+	char *hex_rep;
+	char *rev_hex;
 
-num = va_arg(list, unsigned int);
+	num = va_arg(list, unsigned int);
 
-if (num == 0)
-return (_write_char('0'));
-if (num < 1)
-return (-1);
-len = base_len(num, 16);
-hex_rep = malloc(sizeof(char) * len + 1);
-if (hex_rep == NULL)
-return (-1);
-for (len = 0; num > 0; len++)
-{
-rem_num = num % 16;
-if (rem_num > 9)
-{
-rem_num = hex_check(rem_num, 'X');
-hex_rep[len] = rem_num;
-}
-else
-hex_rep[len] = rem_num + 48;
-num = num / 16;
-}
-hex_rep[len] = '\0';
-rev_hex = rev_string(hex_rep);
-if (rev_hex == NULL)
-return (-1);
-write_base(rev_hex);
-free(hex_rep);
-free(rev_hex);
-return (len);
+	if (num == 0)
+		return (_write_char('0'));
+	if (num < 1)
+		return (-1);
+	len = base_len(num, 16);
+	hex_rep = malloc(sizeof(char) * len + 1);
+	if (hex_rep == NULL)
+		return (-1);
+	for (len = 0; num > 0; len++)
+	{
+		rem_num = num % 16;
+		if (rem_num > 9)
+		{
+			rem_num = hex_check(rem_num, 'X');
+			hex_rep[len] = rem_num;
+		}
+		else
+			hex_rep[len] = rem_num + 48;
+		num = num / 16;
+	}
+	hex_rep[len] = '\0';
+	rev_hex = rev_string(hex_rep);
+	if (rev_hex == NULL)
+		return (-1);
+	write_base(rev_hex);
+	free(hex_rep);
+	free(rev_hex);
+	return (len);
 }
 
 /**
@@ -181,14 +181,14 @@ return (len);
  */
 int hex_check(int num, char x)
 {
-char *hex = "abcdef";
-char *Hex = "ABCDEF";
+	char *hex = "abcdef";
+	char *Hex = "ABCDEF";
 
-num = num - 10;
-if (x == 'x')
-return (hex[num]);
-else
-return (Hex[num]);
-return (0);
+	num = num - 10;
+	if (x == 'x')
+		return (hex[num]);
+	else
+		return (Hex[num]);
+	return (0);
 }
 
